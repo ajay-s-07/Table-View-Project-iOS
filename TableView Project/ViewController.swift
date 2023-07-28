38fc81bcaea8ae6eacf34135eacc7efffb3b2e53
Ajay Sarkate <ajusarkate2002@gmail.com>
2023-07-28 12:08:34 +0530

    Initial Commit

---
+++ TableView Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView Project
+//
+//  Created by Ajay Sarkate on 28/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
